71544c6f13e7fcdce47c69a4428fd9345f6ca9a7
unknown <nhatthach2703@gmail.com>
2025-06-03 19:58:48 +0700

    reset

--- App.tsx
+++ App.tsx
@@ -1,18 +1,8 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import ChatList from './screens/ChatList';
-import ChatRoom from './screens/ChatRoom';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="ChatList">
-        <Stack.Screen name="ChatList" component={ChatList} />
-        <Stack.Screen name="ChatRoom" component={ChatRoom} />
-      </Stack.Navigator>
-    </NavigationContainer>
   );
 }
--- utils/socket.ts
+++
@@ -1,3 +0,0 @@
-import { API_URL } from './api';
-import { io } from 'socket.io-client';
-export const socket = io(API_URL); // dùng IP thật khi chạy thiết bị thật
